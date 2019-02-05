0644990c94406d263303854be41af874efcc8b70
Jupiter Bakakeu <jupiterbak2002@yahoo.fr>
2019-02-05 10:08:51 +0100

    Remove spelling

---
+++ OpenAIGym/EFlexAgent/README.md
@@ -0,0 +1,1 @@
+# EFlex Agent
--- OpenAIGym/EFlexAgent/setup.py
+++ OpenAIGym/EFlexAgent/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name='gym_eflexagent',
+setup(name='gym-eflex-agent',
       version='0.0.1',
       install_requires=['gym']  # And any other dependencies foo needs
 )
